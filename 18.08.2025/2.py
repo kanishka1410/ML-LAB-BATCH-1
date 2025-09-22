88e1182225b180c870302c50a1c11f2bec5d19ef
Kanishka S <kanijeya801@gmail.com>
2025-09-22 14:02:36 +0530

    2.py

---
+++ 18.08.2025/2.py
@@ -0,0 +1,13 @@
+Saving Multiclass Diabetes Dataset.csv to Multiclass Diabetes Dataset (8).csv
+Index(['Gender', 'AGE', 'Urea', 'Cr', 'HbA1c', 'Chol', 'TG', 'HDL', 'LDL',
+       'VLDL', 'BMI', 'Class'],
+      dtype='object')
+KNN Performance on Iris Dataset:
+
+    K  Uniform  Distance
+0   1   0.8125    0.8125
+1   3   0.8500    0.8625
+2   5   0.8625    0.8750
+3   7   0.8000    0.8500
+4   9   0.8125    0.8375
+5  11   0.8000    0.8375
